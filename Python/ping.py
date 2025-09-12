290237e45bcc60a18b0c611825bb4a9e69227bcf
Leonardo Ramos <156909723+leoramosxd@users.noreply.github.com>
2025-09-12 15:00:59 -0400

    ping

---
+++ Python/ping.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from scapy.all import *
+conf.verb = 0
+print "Hosts vivos"
+
+for ip in range(1,255):
+    iprange = "192.168.0.%s" %ip
+    pIP = IP(dst=iprange)
+    pacote = pIP/ICMP()
+    resp, noresp = sr(pacote, timeout=1)
+    for resposta in resp:
+        print resposta[1][IP].src
